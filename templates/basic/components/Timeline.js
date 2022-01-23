a71f701a12d0ea89356679fc2b78273f40266545
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2022-01-23 18:50:07 +0530

    edited tailwind

--- templates/basic/components/Timeline.js
+++ templates/basic/components/Timeline.js
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 const Divider = () => {
   return (
     <div className="border border-gray-200 dark:border-gray-600 w-full my-8" />
@@ -25,8 +23,7 @@ const Step = ({ title, children }) => {
             stroke="currentColor"
             strokeWidth="2"
             strokeLinecap="round"
-            strokeLinejoin="round"
-          >
+            strokeLinejoin="round">
             <path d="M22 11.08V12a10 10 0 11-5.93-9.14" />
             <path d="M22 4L12 14.01l-3-3" />
           </g>
@@ -38,115 +35,7 @@ const Step = ({ title, children }) => {
   );
 };
 
-const FullTimeline = () => (
-  <>
-    <Divider />
-    <Year>2018</Year>
-    <ul>
-      <Step title="Internship - Intel 🤘🏻">
-        Got an Internship at Intel as a Machine Learning Intern. Developed a
-        Pothole Detection System & Face Averaging system using Machine Learning.
-      </Step>
-      <Step title="Entrepreneurial Workshops 🔩">
-        Organised workshops (Part of E-Cell) on topics like Entrepreneurship and
-        Web Development.
-      </Step>
-      <Step title="Full-Stack Journey 🔩">
-        Learnt NodeJS, MongoDB, MySQL along with React and the best practices to
-        build products and web apps which can scale easily.
-      </Step>
-    </ul>
-    <Divider />
-    <Year>2016</Year>
-    <ul>
-      <Step title="Startup - Aceternity 💣">
-        Started a web development and digital marketing agency from scratch.
-        Generated revenue and designed websites for clients.
-      </Step>
-      <Step title="React Enthusiast ⚒">
-        Was introducted to React, took up a course on Udemy to get the hang of
-        it. I haven't stopped since then.
-      </Step>
-      <Step title="Open Source Contributions ⚙️">
-        Started contributing to open source on a daily basis. Contributed to
-        smaller projects to learn more on the contributing part.
-      </Step>
-    </ul>
-    <Divider />
-    <Year>2015</Year>
-    <ul>
-      <Step title="Started College 🔥">
-        Opted for Computer Science B.Tech Degree from University of Petroleum
-        and Energy Studies, Dehradun.
-      </Step>
-      <Step title="Introduced to Open Source ⚡️">
-        My Domain was Open Source Software and Open Standards. I got to know
-        about Free Software Foundation, Git and GitHub and Linux systems.
-      </Step>
-      <Step title="jQuery fanboy 🙌🏻">
-        Started working with jQuery, I was amazed by the level of simplicity it
-        provided back then.
-      </Step>
-      <Step title="C and C++ 🙌🏻">
-        Started learning logics and basic programming fundamentals with Object
-        Oriented Programming. C was intimidating, C++ was fun.
-      </Step>
-      <Step title="First Back 😂">
-        I consider this as an achievement. If you're a CS Engineer, you must
-        have a back in Mathematics.
-      </Step>
-    </ul>
-    <Divider />
-    <Year>2014</Year>
-    <ul>
-      <Step title="Completed 12th Grade 📜">
-        Completed 12th grade with 93.6% Marks in Science stream. Again, a big
-        achievement for me.
-      </Step>
-      <Step title="Developed interest in Web Development 🚀">
-        One of my friends introduced me to Web Development. I started off by
-        making simple HTML, CSS websites with a keen interest in design.
-      </Step>
-    </ul>
-    <Divider />
-    <Year>2012</Year>
-    <ul>
-      <Step title="Completed 10th Grade 📜">
-        Scored 9/10 CGPA in 10th Grade. For me it was huge.
-      </Step>
-      <Step title="Wanted to take up Humanities 🌪❤️">
-        I always wanted to become a Civil Servant. Though my parents didn't
-        agree and that let me to opt for Science stream.
-      </Step>
-      <Step title="Theatre and Drama 💂🏼‍♀️">
-        Acted in my first ever skit in school.
-      </Step>
-    </ul>
-    <Divider />
-    <Year>2008</Year>
-    <ul>
-      <Step title="First Computer 💻">
-        I played GTA San Andreas all day, everyday.
-      </Step>
-    </ul>
-    <Divider />
-    <Year>2001</Year>
-    <ul>
-      <Step title="Moved to Jaipur, Rajasthan 🌆">
-        We moved to Jaipur from our Hometown.
-      </Step>
-    </ul>
-    <Divider />
-    <Year>1998</Year>
-    <ul>
-      <Step title="An Enginner was Born in India 👶🏼 🍼" />
-    </ul>
-  </>
-);
-
 export default function Timeline() {
-  const [isShowingFullTimeline, showFullTimeline] = useState(false);
-
   return (
     <>
       <h3 className="font-bold text-2xl md:text-4xl tracking-tight mb-4 mt-8 text-black dark:text-white">
@@ -154,9 +43,12 @@ export default function Timeline() {
       </h3>
       <Year>2021</Year>
       <ul>
-      <Step title="Built Tailwind Master Kit 🪁">
-          Built <a href="https://www.tailwindmasterkit.com">TailwindMasterKit</a>
-          , A SaaS marketplace for beautiful, handcrafted Website components built with and for Tailwind supported websites. Built the components from the ground up for React and HTML in 30+ categories.
+        <Step title="Built Tailwind Master Kit 🪁">
+          Built{" "}
+          <a href="https://www.tailwindmasterkit.com">TailwindMasterKit</a>, A
+          SaaS marketplace for beautiful, handcrafted Website components built
+          with and for Tailwind supported websites. Built the components from
+          the ground up for React and HTML in 30+ categories.
         </Step>
         <Step title="Created covidrescue.co.in 💊">
           Created <a href="https://www.covidrescue.co.in">covidrescue.co.in</a>
@@ -169,7 +61,7 @@ export default function Timeline() {
           area and city.
         </Step>
       </ul>
-
+      <Divider />
       <Year>2020</Year>
       <ul>
         <Step title="Joined mroads 🎉">
@@ -210,31 +102,6 @@ export default function Timeline() {
           Learnt all the Computer Science subjects again from scratch.
         </Step>
       </ul>
-      {isShowingFullTimeline ? (
-        <FullTimeline />
-      ) : (
-        <button
-          type="button"
-          className="flex items-center text-sm my-4 mx-auto px-4 py-2 rounded-md font-medium text-gray-900 dark:text-gray-100"
-          onClick={() => showFullTimeline(true)}
-        >
-          See More
-          <svg
-            className="h-4 w-4 ml-1"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M19 9l-7 7-7-7"
-            />
-          </svg>
-        </button>
-      )}
     </>
   );
 }
--- templates/basic/pages/index.tsx
+++ templates/basic/pages/index.tsx
@@ -1,5 +1,4 @@
 import Container from "../components/Container";
-import Link from "next/link";
 
 import ProjectCard from "../components/ProjectCard";
 import Timeline from "../components/Timeline";
@@ -10,20 +9,12 @@ import { shuffleArray } from "../lib/shuffleArray";
 import { useEffect, useState } from "react";
 import { RainbowHighlight } from "../ui/RainbowHighlight";
 
-import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
+import { RoughNotationGroup } from "react-rough-notation";
 import { useIsFontReady } from "../lib/useIsFontReady";
-
-import { useTheme } from "next-themes";
 
 export default function Home() {
   const [colors, setColors] = useState([]);
   const isFontReady = useIsFontReady();
-  const { theme, setTheme } = useTheme();
-
-  const play = () => {
-    setTheme(theme === "dark" ? "light" : "dark");
-  };
-
   useEffect(() => {
     setColors(shuffleArray(LIGHT_COLORS));
   }, []);
@@ -43,31 +34,18 @@ export default function Home() {
               </h1>
 
               <h2 className="text-gray-600 dark:text-gray-400 mb-16 mt-4 font-light tracking-wide leading-normal">
-                🖥 I’m a developer, writer, and a creator. <br />
-                🗒 I work at{" "}
-                <span className="font-bold">
-                  <span className="text-blue-500">m</span>roads
-                </span>{" "}
-                as a{" "}
-                <span className="dark:text-white font-bold">
-                  Software Engineer.{" "}
-                </span>{" "}
-                <br />
-                {/* 🤗 You've discovered my piece on the internet.
-                <br />  */}
-                🔨 Building and 🧑🏻‍🏫 Love to teach programming at 🐥 I{" "}
-                <a
-                  className="font-medium text-gray-700 transition-colors hover:text-gray-900 focus:text-gray-900 focus:outline-none dark:text-gray-300 dark:hover:text-gray-600"
-                  target="__blank"></a>{" "}
-                about technology, learning and{" "}
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+                minima vitae quos ducimus dolorem dignissimos rem aperiam enim
+                pariatur tempore?
               </h2>
             </RoughNotationGroup>
           </div>
 
           <div className="md:flex hidden md:w-1/4 flex-col">
             <img
-              src="avatar-new.png"
+              src="/avatar.png"
               className="rounded-full max-w-[200px] shadow-xl shadow-cyan-500/50"
+              alt=""
             />
           </div>
         </div>
@@ -82,70 +60,24 @@ export default function Home() {
           title="Tailwind Master Kit"
           description="Beautiful, Handcrafted, ready-to-use components and templates for your next Tailwind web app project."
           href="https://tailwindmasterkit.com/"
-          icon="tailwindmasterkit"
         />
         <ProjectCard
           title="Covid Rescue"
           description="Get Real-time verified leads on Oxygen, Beds, Remdesivir and more with location and resource filtering"
           href="https://covidrescue.co.in/"
-          icon="covidrescue"
         />
         <ProjectCard
           title="PlaceholderTech"
           description="We build modern, blazing-fast web applications which helps your business grow and increase sales."
           href="https://placeholdertech.in/"
-          icon="placeholdertech"
         />
         <ProjectCard
           title="Feedmeback"
           description="The easiest way to add comments or reviews to your static site. Built as part of React 2025."
           href="https://feedmeback-beta.vercel.app/"
-          icon="fastfeedback"
-        />
-        <Link href="/projects">
-          <a
-            type="button"
-            className="flex items-center text-sm my-4 mx-auto px-4 py-2 rounded-md font-medium text-gray-900 dark:text-gray-100">
-            See More
-            <svg
-              className="h-4 w-4 ml-1"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor">
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M19 9l-7 7-7-7"
-              />
-            </svg>
-          </a>
-        </Link>
-
-        <h3 className="font-bold text-2xl md:text-4xl tracking-tight mb-4 mt-8 text-black dark:text-white">
-          Upcoming Projects
-        </h3>
-        <ProjectCard
-          title="Algochurn"
-          description="Your last moment friend before a Technical Interview round. Practice the most popular Data Structures & Algorithms"
-          href="#"
-          icon="algochurn"
-        />
-        <ProjectCard
-          title="VSCode Resume"
-          description="A VSCode themed resume for all the web developers out there. A UI which looks exactly like a React file-system based VSCode window with create and update operations."
-          href="#"
-          icon="vscode"
-        />
-        <ProjectCard
-          title="More projects coming soon.."
-          description="I get ideas all day 🙄, All of them are updated here as soon as I start working on them."
-          href="#"
-          icon="more"
         />
         <Timeline />
-        <h1>Contact Page</h1>
+        <h1 className="text-green-200">Contact Page</h1>
       </div>
     </Container>
   );
